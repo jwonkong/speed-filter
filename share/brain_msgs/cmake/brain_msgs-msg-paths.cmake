f4154c12c80403a55b1f8f021eb37383db143d6f
Jiwon Kong <96765798+Jiwon-Kong@users.noreply.github.com>
2024-02-22 14:47:22 +0900

    Add files via upload

---
+++ share/brain_msgs/cmake/brain_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${brain_msgs_DIR}/.." "msg" brain_msgs_MSG_INCLUDE_DIRS UNIQUE)
+set(brain_msgs_MSG_DEPENDENCIES std_msgs;nav_msgs;geometry_msgs)
